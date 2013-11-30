6e39a56065d6474131d5fbc16f0b92784677d59e
Fredrik Holmqvist <fredrik.holmqvist@gmail.com>
2013-11-30 21:44:03 +0100

    Remove old EFI string writing test code.

--- src/system/boot/platform/efi/start.cpp
+++ src/system/boot/platform/efi/start.cpp
@@ -100,16 +100,6 @@ efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systemTable)
 
   //launch kernel (main(&args);)
 
-/*
-	UINTN index;
-	EFI_EVENT event = systemTable->ConIn->WaitForKey;
-
-	SIMPLE_INPUT_INTERFACE *conIn = systemTable->ConIn;
-	SIMPLE_TEXT_OUTPUT_INTERFACE *conOut = systemTable->ConOut;
-	conOut->OutputString(conOut, exampleText);
-
-	systemTable->BootServices->WaitForEvent(1, &event, &index);
-*/
 	console_wait_for_key();
 	return EFI_SUCCESS;
 }

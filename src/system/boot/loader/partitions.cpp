6e8f0afdc1b34eebc05e7b05acac0f5c11d77e9a
François Revol <revol@free.fr>
2013-12-07 07:47:19 +0100

    loader: Drop the HashMap for partition lookup by id
    The HashMap constructor was called before the heap is initialized,
    ending up calling malloc from the OpenHashMap constructor.
    
    Oddly it was still working on x86 but broke other platforms.
    
    Instead we add a Lookup() static method to Partition,
    which by default walks gPartitions for the id,
    and recursively calls itself on the children lists.
    
    This means we must add a partition even temporarily to gPartitions
    before Scan()ing it though.
    
    Signed-off-by: François Revol <revol@free.fr>

--- src/system/boot/loader/partitions.cpp
+++ src/system/boot/loader/partitions.cpp
@@ -16,7 +16,6 @@
 #include <boot/stdio.h>
 #include <boot/vfs.h>
 #include <ddm_modules.h>
-#include <HashMap.h>
 
 #include "RootFileSystem.h"
 
@@ -101,7 +100,6 @@ private:
 
 
 static int32 sIdCounter = 0;
-static HashMap<HashKey32<int32>, Partition*> sIdPartitionMap;
 
 
 //	#pragma mark -
@@ -118,7 +116,6 @@ Partition::Partition(int fd)
 	memset((partition_data *)this, 0, sizeof(partition_data));
 
 	id = atomic_add(&sIdCounter, 1);
-	sIdPartitionMap.Put(id, this);
 
 	// it's safe to close the file
 	fFD = dup(fd);
@@ -139,8 +136,30 @@ Partition::~Partition()
 			child->SetParent(NULL);
 	}
 
-	sIdPartitionMap.Remove(id);
 	close(fFD);
+}
+
+
+Partition *
+Partition::Lookup(partition_id id, NodeList *list)
+{
+	Partition *p;
+
+	if (list == NULL)
+		list = &gPartitions;
+
+	NodeIterator iterator = list->GetIterator();
+
+	while ((p = (Partition *)iterator.Next()) != NULL) {
+		if (p->id == id)
+			return p;
+		if (!p->fChildren.IsEmpty()) {
+			Partition *c = Lookup(id, &p->fChildren);
+			if (c)
+				return c;
+		}
+	}
+	return NULL;
 }
 
 
@@ -321,7 +340,7 @@ Partition::Scan(bool mountFileSystems, bool isBootDevice)
 		if (priority < 0.0)
 			continue;
 
-		TRACE(("  priority: %ld\n", (int32)(priority * 1000)));
+		TRACE(("  priority: %" B_PRId32 "\n", (int32)(priority * 1000)));
 		if (priority <= bestPriority) {
 			// the disk system recognized the partition worse than the currently
 			// best one
@@ -379,9 +398,9 @@ Partition::Scan(bool mountFileSystems, bool isBootDevice)
 		Partition *child = NULL;
 
 		while ((child = (Partition *)iterator.Next()) != NULL) {
-			TRACE(("%p Partition::Scan(): scan child %p (start = %Ld, size "
-				"= %Ld, parent = %p)!\n", this, child, child->offset,
-				child->size, child->Parent()));
+			TRACE(("%p Partition::Scan(): scan child %p (start = %" B_PRIdOFF
+				", size = %" B_PRIdOFF ", parent = %p)!\n", this, child,
+				child->offset, child->size, child->Parent()));
 
 			child->Scan(mountFileSystems);
 
@@ -439,16 +458,19 @@ add_partitions_for(int fd, bool mountFileSystems, bool isBootDevice)
 	partition->block_size = 512;
 	partition->size = partition->Size();
 
-	// add this partition to the list of partitions, if it contains
-	// or might contain a file system
+	// add this partition to the list of partitions
+	// temporarily for Lookup() to work
+	gPartitions.Add(partition);
+
+	// keep it, if it contains or might contain a file system
 	if ((partition->Scan(mountFileSystems, isBootDevice) == B_OK
 			&& partition->IsFileSystem())
 		|| (!partition->IsPartitioningSystem() && !mountFileSystems)) {
-		gPartitions.Add(partition);
 		return B_OK;
 	}
 
 	// if not, we no longer need the partition
+	gPartitions.Remove(partition);
 	delete partition;
 	return B_OK;
 }
@@ -466,7 +488,7 @@ add_partitions_for(Node *device, bool mountFileSystems, bool isBootDevice)
 
 	status_t status = add_partitions_for(fd, mountFileSystems, isBootDevice);
 	if (status < B_OK)
-		dprintf("add_partitions_for(%d) failed: %ld\n", fd, status);
+		dprintf("add_partitions_for(%d) failed: %" B_PRId32 "\n", fd, status);
 
 	close(fd);
 	return B_OK;
@@ -477,7 +499,7 @@ partition_data *
 create_child_partition(partition_id id, int32 index, off_t offset, off_t size,
 	partition_id childID)
 {
-	Partition *partition = sIdPartitionMap.Get(id);
+	Partition *partition = Partition::Lookup(id);
 	if (partition == NULL) {
 		dprintf("creating partition failed: could not find partition.\n");
 		return NULL;
@@ -505,7 +527,7 @@ get_child_partition(partition_id id, int32 index)
 {
 	// TODO: do we really have to implement this?
 	//	The intel partition module doesn't really need this for our mission...
-	TRACE(("get_child_partition(id = %lu, index = %ld)\n", id, index));
+	TRACE(("get_child_partition(id = %" B_PRIu32 ", index = %" B_PRId32 ")\n", id, index));
 
 	return NULL;
 }
@@ -514,7 +536,7 @@ get_child_partition(partition_id id, int32 index)
 partition_data *
 get_parent_partition(partition_id id)
 {
-	Partition *partition = sIdPartitionMap.Get(id);
+	Partition *partition = Partition::Lookup(id);
 	if (partition == NULL) {
 		dprintf("could not find parent partition.\n");
 		return NULL;

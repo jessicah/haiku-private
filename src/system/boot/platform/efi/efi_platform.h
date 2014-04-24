7239a52517b440b8e8e8c61e94fdaaa4e5bfa554
Jessica Hamilton <jessica.l.hamilton@gmail.com>
2014-04-24 18:01:32 +1200

    EFI: add simple accessors for boot & runtime services

--- src/system/boot/platform/efi/efi_platform.h
+++ src/system/boot/platform/efi/efi_platform.h
@@ -10,11 +10,14 @@
 #include "efibind.h"
 #include "efidef.h"
 #include "efidevp.h"
+#include "efiprot.h"
 #include "eficon.h"
 #include "efierr.h"
 #include "efiapi.h"
 
 
-extern const EFI_SYSTEM_TABLE *kSystemTable;
+extern const EFI_SYSTEM_TABLE		*kSystemTable;
+extern const EFI_BOOT_SERVICES		*kBootServices;
+extern const EFI_RUNTIME_SERVICES	*kRuntimeServices;
 
 #endif	/* EFI_PLATFORM_H */

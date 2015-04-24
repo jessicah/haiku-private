0f9fe5dc237de14f7b2f261590a456699d9adb09
Fredrik Holmqvist <fredrik.holmqvist@gmail.com>
2015-04-24 21:25:48 +1200

    UEFI: platform headers

---
+++ headers/private/kernel/boot/platform/efi/platform_stage2_args.h
@@ -0,0 +1,13 @@
+#ifndef _KERNEL_BOOT_PLATFORM_EFI_STAGE2_ARGS_H
+#define _KERNEL_BOOT_PLATFORM_EFI_STAGE2_ARGS_H
+
+#ifndef KERNEL_BOOT_STAGE2_ARGS_H
+#	error This file is included from <boot/stage2_args.h> only
+#endif
+
+#warning EFI: IMPLEMENT platform_stage2_args.h
+
+struct platform_stage2_args {
+};
+
+#endif /* _KERNEL_BOOT_PLATFORM_EFI_STAGE2_ARGS_H */
---
+++ src/system/boot/platform/efi/efi_platform.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2013, Fredrik Holmqvist, fredrik.holmqvist@gmail.com. All rights reserved.
+ * Distributed under the terms of the Haiku License.
+ */
+
+
+#ifndef EFI_PLATFORM_H
+#define EFI_PLATFORM_H
+
+#include "efibind.h"
+#include "efidef.h"
+#include "efidevp.h"
+#include "eficon.h"
+#include "efierr.h"
+#include "efiapi.h"
+
+
+extern const EFI_SYSTEM_TABLE *kSystemTable;
+
+#endif	/* EFI_PLATFORM_H */
